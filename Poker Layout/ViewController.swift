23bae3b2ed31a2d509738344c20b2485255f4684
Justin Gluska <justingluska@gmail.com>
2019-08-08 00:41:09 -0400

    Layout Changes

--- Poker Layout/ViewController.swift
+++ Poker Layout/ViewController.swift
@@ -15,11 +15,20 @@ class ViewController: UIViewController, GADBannerViewDelegate {
     @IBOutlet weak var bannerView: GADBannerView!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var helpButton: UIButton!
-
+    @IBOutlet weak var sButt: UIButton!
+    @IBOutlet weak var hButt: UIButton!
+    
     var banner: GADBannerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        sButt.clipsToBounds = true
+        sButt.layer.cornerRadius = 8
+        hButt.clipsToBounds = true
+        hButt.layer.cornerRadius = 15
+        //startButton.clipsToBounds = true
+        //startButton.layer.cornerRadius = 15
+        //helpButton.layer.cornerRadius = 15
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.adSize = kGADAdSizeSmartBannerPortrait
         bannerView.delegate = self
